0f1dee659417c3ce1168668e222d1329fbb72b27
whododatgoju <64809790+whododatgoju@users.noreply.github.com>
2025-01-16 13:44:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title("My Streamlit App")
-st.write("Welcome to my first Streamlit app deployed on Streamlit Cloud!")
+st.title("My Updated Streamlit App")
+st.write("This is an updated version of my Streamlit app.")
