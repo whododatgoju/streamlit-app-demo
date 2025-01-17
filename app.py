8a08abc80668de2495cb831e023361d2a67fa123
whododatgoju <64809790+whododatgoju@users.noreply.github.com>
2025-01-16 21:38:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,6 @@ st.text_area('Area for textual entry')
 st.date_input('Date input')
 st.time_input('Time entry')
 st.file_uploader('File uploader')
-st.download_button('On the dl', data)
 st.camera_input("一二三,茄子!")
 st.color_picker('Pick a color')
 
